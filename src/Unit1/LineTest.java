d14aac4b48db5606143d488c9e2f69ef3744d39c
FJKLH1 <3186531875@qq.com>
2020-09-22 16:17:09 +0800

    sd

---
+++ src/Unit1/LineTest.java
@@ -0,0 +1,26 @@
+package Unit1;
+
+public class LineTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
+
+//next-line
+/*public class Test
+ *{
+ *  public static void main(String[] args)
+ *  {
+ *  System.out.println("Block Styles");
+ *  }
+ *} */
+
+
+//end-of-line
+/*public class Test {
+ *  public static void main(String[] args) {
+ *  System.out.println("Block Styles");
+ *  }
+ *} */
---
+++ src/Unit1/Welcome.java
@@ -0,0 +1,19 @@
+package Unit1;
+
+public class Welcome {
+	public static void main(String[] args) {
+	//Display message Welcome to Java! on the console
+	System.out.println("Welcome to Java!");		
+	}
+
+}
+
+
+//三种注释的例子
+
+//This application program displays Welcome to Java!
+
+/*This application program displays Welcome to Java!*/
+
+/*This application program 
+  displays Welcome to Java!*/
